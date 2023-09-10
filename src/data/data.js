7a22559e198c00415faa2ae87ac6a3bd465cdee9
“Abdul <kaazees31@gmail.com>
2023-09-10 12:02:59 +0530

    portfolio

---
+++ src/data/data.js
@@ -0,0 +1,41 @@
+import Bike from "../assets/projects/Bikerental.jpg";
+import Online from "../assets/projects/Onlineshopping.jpg";
+import movie from "../assets/projects/movie.png";
+import ecom from "../assets/projects/Ecommerce.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "Bike Rental-app",
+    image: Bike,
+    github:
+      "https://github.com/Abdulazeessahulfarithka/rentbike-portal-frontend",
+    backend:
+      "https://github.com/Abdulazeessahulfarithka/rentbike-portal-backend",
+    live: "https://bikebuddy1.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Online Shopping",
+    image: Online,
+    github: "https://github.com/Abdulazeessahulfarithka/frontend-webscrap",
+    backend: "https://github.com/Abdulazeessahulfarithka/webscrapping-backend",
+    live: "https://fantastic-sprinkles-598d21.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Movie App(MERN)",
+    image: movie,
+    github: "https://github.com/boobalan08/React-Movies_App",
+    backend: "https://github.com/boobalan08/express",
+    live: "https://movie-app-reacts.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "Ecommerce App(MERN)",
+    image: ecom,
+    github: "https://github.com/boobalan08/ecommerce-frontend",
+    backend: "https://github.com/boobalan08/ecommerce-backend",
+    live: "https://asia-ecom-shop.netlify.app/",
+  },
+];
